3b63ce8fae10131b532b203e205524372ba77ae2
terasakisatoshi <terasakisatoshi.math>
2016-09-06 14:21:56 +0900

    calc pi and pass to master its value

---
+++ MPISingle/calcavgpiusingmpireduce.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <mpi.h>
+
+double myrand(void){
+	return (double)rand()/(double)RAND_MAX;
+}
+
+double calc_pi(int seed,int trial){
+	srand(seed);
+	int n=0;
+	for (int i = 0; i < trial; ++i)
+	{
+		double x=myrand();
+		double y=myrand();
+		if(x*x+y*y<1){
+			n++;
+		}
+	}
+	return 4.0*(double)n/(double)trial;
+}
+
+int main(int argc,char ** argv){
+	int master=0;
+	int trial=10000000;
+	MPI_Init(&argc,&argv);
+	int rank,procs;
+	MPI_Comm_size(MPI_COMM_WORLD,&procs);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	double pi=calc_pi(rank,trial);
+	printf("myrank is %d of procs %d mypi is %f\n",rank,procs,pi);
+	MPI_Barrier(MPI_COMM_WORLD);
+	double sum=0;
+	MPI_Reduce(&pi,&sum,1,MPI_DOUBLE,MPI_SUM,master,MPI_COMM_WORLD);
+	sum=sum/double(procs);
+	if(0==rank){
+		printf("average=%f\n", sum);
+	}
+	MPI_Finalize();
+	return 0;
+}
