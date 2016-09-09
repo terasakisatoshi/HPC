de2b5ff88cdcf95f623f739c3b79fc59c70f95f3
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2016-09-09 21:26:08 +0900

    add reduce and all reduce code

---
+++ PythonMPI/reduceall.py
@@ -0,0 +1,15 @@
+from mpi4py import MPI 
+
+comm=MPI.COMM_WORLD
+rank=comm.Get_rank()
+sendmsg=rank
+
+print("I am rank {} I have sendmsg as {}".format(rank,sendmsg))
+recvmsg1=comm.reduce(sendmsg,op=MPI.SUM,root=0)
+
+if rank==0:
+    print(recvmsg1)
+
+print("all reduce at rank {}".format(rank))
+recvmsg2=comm.allreduce(sendmsg,op=MPI.SUM)
+print("I am rank {} I have recvmsg2 op=MPI.SUM as {}".format(rank,recvmsg2))
